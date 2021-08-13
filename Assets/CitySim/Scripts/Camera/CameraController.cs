b2e806d8509b0525329735819175d7d6b2346b4d
Martin Sederholm <sederholm.martin@gmail.com>
2021-08-13 16:47:21 +0300

    Make camera offset smooth curve
    - Camera offset from target follows smooth curve when zooming
    - Reduce annoying shaking when zooming in completely

--- Assets/CitySim/Scripts/Camera/CameraController.cs
+++ Assets/CitySim/Scripts/Camera/CameraController.cs
@@ -5,11 +5,14 @@ namespace Unity.CitySim.Camera
 {
     public class CameraController : MonoBehaviour
     {
-        [Range(1, 100)]
-        public float startVerticalOffset = 10f;
+        [Range(1f, 100f)]
+        public float startHorizontalOffset = 10f;
 
-        [Range(1, 100)]
-        public float maxHorizontalOffset = 10f;
+        [Range(1, 10)]
+        public int verticalExponent = 3;
+
+        [Range(1f, 10f)]
+        public float elevationDistance = 4f;
 
         [Range(0.1f, 10f)]
         public float zoomSensitivity = 1f;
@@ -17,10 +20,12 @@ namespace Unity.CitySim.Camera
         [Range(1f, 10f)]
         public float zoomSpeed = 1f;
 
-        float verticalOffset;
-        float targetVerticalOffset;
+        [Range(0f, 1f)]
+        public float minZoomStep = 0.1f;
+
         float horizontalOffset;
         float targetHorizontalOffset;
+        float verticalOffset;
         GameObject cameraTarget;
         MapGenerator mapGenerator;
 
@@ -32,10 +37,9 @@ namespace Unity.CitySim.Camera
             if (!cameraTarget)
                 throw new System.Exception("Player not found!");
 
-            verticalOffset = startVerticalOffset;
-            targetVerticalOffset = verticalOffset;
-            horizontalOffset = maxHorizontalOffset;
+            horizontalOffset = startHorizontalOffset;
             targetHorizontalOffset = horizontalOffset;
+            verticalOffset = horizontalOffset * Mathf.Pow(horizontalOffset / elevationDistance, verticalExponent);
         }
 
         void Update()
@@ -45,28 +49,20 @@ namespace Unity.CitySim.Camera
             float zoom = -Input.mouseScrollDelta.y * zoomSensitivity;
 
             // If zooming in
-            if (zoom < 0) {
-                targetVerticalOffset += zoom;
-                // If we are at the lowest allowed vertical offset
-                if (targetVerticalOffset < 0) {
-                    targetHorizontalOffset = System.Math.Max(targetHorizontalOffset + targetVerticalOffset, 0);
-                    targetVerticalOffset = 0;
-                }
-            }
-
+            if (zoom < 0)
+                targetHorizontalOffset = Mathf.Max(targetHorizontalOffset + zoom, 0);
             // If zooming out
-            if (zoom > 0) {
+            else if (zoom > 0)
                 targetHorizontalOffset += zoom;
-                // If we are at highest allowed horizontal offset
-                if (targetHorizontalOffset > maxHorizontalOffset) {
-                    targetVerticalOffset += targetHorizontalOffset - maxHorizontalOffset;
-                    targetHorizontalOffset = maxHorizontalOffset;
-                }
-            }
 
             // Smoothen camera movement during zooming
-            verticalOffset += (targetVerticalOffset - verticalOffset) * zoomSpeed * Time.deltaTime;
-            horizontalOffset += (targetHorizontalOffset - horizontalOffset) * zoomSpeed * Time.deltaTime;
+            if (Mathf.Abs(targetHorizontalOffset - horizontalOffset) < minZoomStep)
+                horizontalOffset = targetHorizontalOffset;
+            else
+                horizontalOffset += (targetHorizontalOffset - horizontalOffset) * zoomSpeed * Time.deltaTime;
+
+            horizontalOffset = Mathf.Max(horizontalOffset, 0);
+            verticalOffset = horizontalOffset * Mathf.Pow(horizontalOffset / elevationDistance, verticalExponent);
 
 
             //// OFFSET ////
